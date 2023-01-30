9288d0c776b5d1595e6bf1439471f4d10861b688
shafiq bayat <bayat37@hotmail.com>
2023-01-30 15:53:17 -0500

    m6 updates

---
+++ m6-stream/src/main/java/com/cydeo/Course.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+public class Course {
+
+    private String courseName;
+    private int courseID;
+
+    public Course(String courseName, int courseID) {
+        this.courseName = courseName;
+        this.courseID = courseID;
+    }
+
+
+
+}
---
+++ m6-stream/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,40 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        Creating Stream from Array
+
+        String[] courses = {"java", "Spring","Agile"};
+        Stream<String> courseStream = Arrays.stream(courses);
+
+
+//        Creating Stream from Collections
+
+        List<String> courseList = Arrays.asList("java", "Spring","Agile");
+
+        Stream<String> courseStream2 = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+          new Course("java", 100),
+          new Course("DS",200),
+          new Course("MS",102));
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
+
+
+
+
+
+
+
+
+    }
+}
