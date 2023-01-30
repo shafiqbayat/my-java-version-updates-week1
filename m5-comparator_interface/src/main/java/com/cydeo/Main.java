7f88c4143173e4eb8630938900c916886199e080
shafiq bayat <bayat37@hotmail.com>
2023-01-30 13:22:54 -0500

    m5 updates

---
+++ m5-comparator_interface/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,20 @@
+package com.cydeo;
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list = Arrays.asList(10, 20, 50, 4, 22, 31);
+
+        Collections.sort(list);
+
+        System.out.println(list);
+
+
+
+    }
+}
