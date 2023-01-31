ed8dc15da4cba1b8bf9e4d0253f556e4f0ba790d
shafiq bayat <bayat37@hotmail.com>
2023-01-31 10:32:52 -0500

    m6 updates

--- m6-stream/src/main/java/com/cydeo/StreamOperation.java
+++ m6-stream/src/main/java/com/cydeo/StreamOperation.java
@@ -3,6 +3,7 @@ package com.cydeo;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Filter;
+import java.util.stream.Stream;
 
 public class StreamOperation {
 
@@ -11,9 +12,35 @@ public class StreamOperation {
 
         List<Integer> list = Arrays.asList(1,5,6,4,5,8,7);
 
-        list.forEach(System.out::println);
+//        list.forEach(System.out::println);
 
 //        Filter
+
+        list.stream().filter(i -> i%2==0)
+                .forEach(System.out::println);
+
+        System.out.println("Filter");
+        System.out.println("Distinct");
+//        Distinct
+        Stream<Integer> str = list.stream().filter(i->i%2==0).distinct();
+        str.forEach(System.out::println); // stream is closed here
+
+        System.out.println("Limit");
+
+        list.stream()
+                .filter(i -> i%2==0)
+                .limit(2)
+                .forEach(System.out::println);
+
+
+
+
+
+
+
+
+
+
 
 
 
