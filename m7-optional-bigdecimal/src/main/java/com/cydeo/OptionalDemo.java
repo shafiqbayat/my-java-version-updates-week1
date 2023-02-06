46ae1b517d62aa82b25694e392922e6d0445202e
shafiq bayat <bayat37@hotmail.com>
2023-02-06 14:57:02 -0500

    m7 - m8 Updates

--- m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -1,6 +1,8 @@
 package com.cydeo;
 
 
+import com.cydeo.java9.FactoryMethodsDemo;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +30,8 @@ public class OptionalDemo {
 
         //orElse
         System.out.println(bigNumber.orElse(5));
+
+        FactoryMethodsDemo f1 = new FactoryMethodsDemo();
 
 
     }
---
+++ m8-new-version-updates/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module modelJava9{
+    exports com.cydeo.java9;
+        }
