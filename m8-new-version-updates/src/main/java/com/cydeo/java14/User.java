13c50bcd0ab8163a0f3f8cd5453ee5bcdd31b5df
shafiq bayat <bayat37@hotmail.com>
2023-02-07 14:10:16 -0500

    m8 Updates

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,15 @@
+package com.cydeo.java14;
+
+public record User(String name,String address) {
+    public User {
+    }
+
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+
+
+}
